296d486f99222cde0d4686af4f10be282a751ce9
zhaowanting <1401325799@qq.com>
2019-12-19 21:55:06 +0800

    能跑

--- utils/process.py
+++ utils/process.py
@@ -4,6 +4,10 @@ import networkx as nx
 import scipy.sparse as sp
 from scipy.sparse.linalg.eigen.arpack import eigsh
 import sys
+from sys import path
+import scipy.sparse
+path.append(r"D:\python\my_mixhop")
+from mixhop_dataset import common_load_data
 
 """
  Prepare adjacency matrix by expanding up to a given neighbourhood.
@@ -41,59 +45,105 @@ def sample_mask(idx, l):
     mask = np.zeros(l)
     mask[idx] = 1
     return np.array(mask, dtype=np.bool)
+#
+# def load_data(dataset_str): # {'pubmed', 'citeseer', 'cora'}
+#     """Load data."""
+#     names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+#     objects = []
+#     for i in range(len(names)):
+#         with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
+#             if sys.version_info > (3, 0):
+#                 objects.append(pkl.load(f, encoding='latin1'))
+#             else:
+#                 objects.append(pkl.load(f))
+#
+#     x, y, tx, ty, allx, ally, graph = tuple(objects)
+#     test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
+#     test_idx_range = np.sort(test_idx_reorder)
+#
+#     if dataset_str == 'citeseer':
+#         # Fix citeseer dataset (there are some isolated nodes in the graph)
+#         # Find isolated nodes, add them as zero-vecs into the right position
+#         test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+#         tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+#         tx_extended[test_idx_range-min(test_idx_range), :] = tx
+#         tx = tx_extended
+#         ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+#         ty_extended[test_idx_range-min(test_idx_range), :] = ty
+#         ty = ty_extended
+#
+#     features = sp.vstack((allx, tx)).tolil()
+#     features[test_idx_reorder, :] = features[test_idx_range, :]
+#     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+#
+#     labels = np.vstack((ally, ty))
+#     labels[test_idx_reorder, :] = labels[test_idx_range, :]
+#
+#     idx_test = test_idx_range.tolist()
+#     idx_train = range(len(y))
+#     idx_val = range(len(y), len(y)+500)
+#
+#     train_mask = sample_mask(idx_train, labels.shape[0])
+#     val_mask = sample_mask(idx_val, labels.shape[0])
+#     test_mask = sample_mask(idx_test, labels.shape[0])
+#
+#     y_train = np.zeros(labels.shape)
+#     y_val = np.zeros(labels.shape)
+#     y_test = np.zeros(labels.shape)
+#     y_train[train_mask, :] = labels[train_mask, :]
+#     y_val[val_mask, :] = labels[val_mask, :]
+#     y_test[test_mask, :] = labels[test_mask, :]
+#
+#     print(adj.shape)
+#     print(features.shape)
+#
+#     return adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
 
-def load_data(dataset_str): # {'pubmed', 'citeseer', 'cora'}
-    """Load data."""
-    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
-    objects = []
-    for i in range(len(names)):
-        with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
-            if sys.version_info > (3, 0):
-                objects.append(pkl.load(f, encoding='latin1'))
-            else:
-                objects.append(pkl.load(f))
 
-    x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
-    test_idx_range = np.sort(test_idx_reorder)
+def load_data(dataset_str):
+    dataset_str='../my_mixhop/data/ind.'+dataset_str
+    #[[], ['PA'], ['PA', 'PA'], ['PA', 'PC'], ['PA', 'PT'], ['PC'], ['PC', 'PC'], ['PC', 'PT'], ['PT'], ['PT', 'PT'], ['PA', 'PA', 'PA', 'PA'], ['PA', 'PC', 'PC', 'PA'], ['PA', 'PT', 'PT', 'PA'], ['PC', 'PC', 'PC', 'PC'], ['PC', 'PT', 'PT', 'PC'], ['PT', 'PT', 'PT', 'PT']]
 
-    if dataset_str == 'citeseer':
-        # Fix citeseer dataset (there are some isolated nodes in the graph)
-        # Find isolated nodes, add them as zero-vecs into the right position
-        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
-        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-        tx_extended[test_idx_range-min(test_idx_range), :] = tx
-        tx = tx_extended
-        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
-        ty_extended[test_idx_range-min(test_idx_range), :] = ty
-        ty = ty_extended
+    num_nodes, edge_sets, metapaths,metapaths_name, train_idx, valid_idx, test_idx, adj_indices, adj_values, allx, ally = common_load_data(dataset_str)
+    print(metapaths)
+    adjs =[]
+    for metapath in metapaths:
+        adj = scipy.sparse.eye(num_nodes)
+        for type in metapath:
+            edges = adj_indices[type]
+            values = adj_values[type]
+            row = [pair[0] for pair in edges]
+            col = [pair[1] for pair in edges]
+            data = [0.000001 for value in values]
+            adj1 = scipy.sparse.csr_matrix((data, (row, col)), shape=(num_nodes, num_nodes))
+            adj = adj * adj1
+            adj = adj.ceil()
+        adjs.append(sp.eye(adj.shape[0])+adj)
+    print('adjs---------------', adjs)
 
-    features = sp.vstack((allx, tx)).tolil()
-    features[test_idx_reorder, :] = features[test_idx_range, :]
-    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
 
-    labels = np.vstack((ally, ty))
-    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+    train_mask =  np.zeros((num_nodes), dtype=np.bool)
+    y_train = np.zeros((num_nodes,ally.shape[1]), dtype=np.float)
+    for node in train_idx:
+        train_mask[node] = True
+        y_train[node] = ally[node]
 
-    idx_test = test_idx_range.tolist()
-    idx_train = range(len(y))
-    idx_val = range(len(y), len(y)+500)
+    valid_mask = np.zeros((num_nodes), dtype=np.bool)
+    y_valid = np.zeros((num_nodes, ally.shape[1]), dtype=np.float)
+    for node in valid_idx:
+        valid_mask[node] = True
+        y_valid[node] = ally[node]
 
-    train_mask = sample_mask(idx_train, labels.shape[0])
-    val_mask = sample_mask(idx_val, labels.shape[0])
-    test_mask = sample_mask(idx_test, labels.shape[0])
+    test_mask = np.zeros((num_nodes), dtype=np.bool)
+    y_test = np.zeros((num_nodes, ally.shape[1]), dtype=np.float)
+    for node in test_idx:
+        test_mask[node] = True
+        y_test[node] = ally[node]
 
-    y_train = np.zeros(labels.shape)
-    y_val = np.zeros(labels.shape)
-    y_test = np.zeros(labels.shape)
-    y_train[train_mask, :] = labels[train_mask, :]
-    y_val[val_mask, :] = labels[val_mask, :]
-    y_test[test_mask, :] = labels[test_mask, :]
 
-    print(adj.shape)
-    print(features.shape)
 
-    return adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
+    return metapaths,metapaths_name, adjs, allx, y_train, y_valid, y_test, train_mask, valid_mask, test_mask
+
 
 def load_random_data(size):
 
@@ -155,6 +205,7 @@ def standardize_data(f, train_mask):
 def preprocess_features(features):
     """Row-normalize feature matrix and convert to tuple representation"""
     rowsum = np.array(features.sum(1))
+    print("rowsum",rowsum)
     r_inv = np.power(rowsum, -1).flatten()
     r_inv[np.isinf(r_inv)] = 0.
     r_mat_inv = sp.diags(r_inv)
